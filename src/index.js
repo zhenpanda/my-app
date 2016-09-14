8fb4593f1b620638c482cf65f4f27d51708bc21d
Zhenting <zhentzhou@gmail.com>
2016-09-14 15:53:29 -0400

    basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+// App root
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+
+import App from './components/App';
+import Featured from './components/Featured'
+import Settings from './pages/Settings'
+
+// ReactDOM.render(<App />,document.getElementById('app'));
+
+const app = document.getElementById('app');
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  //routes
+  <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Featured}></IndexRoute>
+        <Route path="settings" component={Settings}></Route>
+      </Route>
+  </Router>,
+app);
